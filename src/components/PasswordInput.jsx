a7d993dd7649afccfcff8566c6dae3632f806f8a
Katya Weicht <katya.weicht@gmail.com>
2025-06-04 21:55:56 -0600

    Create handleChange function

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,8 +1,11 @@
 // Code PasswordInput Component Here
 
 function PasswordInput (){
+    function handleChange() {
+        console.log("Entering password...")
+    }
     return(
-        <input type="password" id="pwd" name="password" onChange={()=>{}}></input>
+        <input type="password" id="pwd" name="password" onChange={handleChange}></input>
     )
 }
 
